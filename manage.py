861b55437350a3496dc660848c060ead59c068aa
Shilee-Aafk <kamilo.uribe07@gmail.com>
2025-11-08 15:14:31 -0300

    Fix: Añadir la ruta del proyecto al PYTHONPATH en manage.py

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import sys
 def main():
     """Run administrative tasks."""
     sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AbbaRestaurate.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AbbaRestaurante.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
